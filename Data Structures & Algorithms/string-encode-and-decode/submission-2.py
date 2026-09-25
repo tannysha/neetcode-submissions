3a8a61c6978797eed6e5b2325103145aee070a26
Tanisha Magikar <157842316+tannysha@users.noreply.github.com>
2026-09-24 19:38:19 -0500

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        return "🫤".join(["😡"] + strs + ["😡"])
+        
+    def decode(self, s: str) -> List[str]:
+        return s.split("🫤") [1:-1]
+
+
+        
+        
+
+        
